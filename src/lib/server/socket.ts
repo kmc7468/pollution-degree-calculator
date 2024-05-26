ef3f58608f7f3d92fb453e2725e2d5b49fe3f9ae
static <kmc7468@naver.com>
2024-05-27 02:41:39 +0900

    서버에서 yolov5 모델을 구동하도록 구현

--- src/lib/server/socket.ts
+++ src/lib/server/socket.ts
@@ -2,14 +2,30 @@ import http from "http";
 import http2 from "http2";
 import { Server } from "socket.io";
 
-export const injectSocketIO = (server: http.Server | http2.Http2SecureServer) => {
+import { loadModel, detect } from "./yolov5";
+
+export const injectSocketIO = async (server: http.Server | http2.Http2SecureServer) => {
+  const model = await loadModel();
+  let running = false;
+  console.log("ready");
+
   const io = new Server(server);
   io.on("connection", (socket) => {
     socket.on("start", (data) => {
       console.log(data);
     });
-    socket.on("frame", (data: string) => {
-      socket.emit("yolo", data); // TODO
+    socket.on("frame", async (data: string) => {
+      if (!running) {
+        running = true;
+
+        const result = await detect(model, data);
+        socket.emit("yolo", {
+          image: data,
+          objects: result
+        });
+
+        running = false;
+      }
     });
   });
 };
---
+++ src/lib/server/yolov5.ts
@@ -0,0 +1,124 @@
+import tf from "@tensorflow/tfjs-node-gpu";
+import sharp from "sharp";
+
+const cocoNames = [
+  "person",
+  "bicycle",
+  "car",
+  "motorcycle",
+  "airplane",
+  "bus",
+  "train",
+  "truck",
+  "boat",
+  "traffic light",
+  "fire hydrant",
+  "stop sign",
+  "parking meter",
+  "bench",
+  "bird",
+  "cat",
+  "dog",
+  "horse",
+  "sheep",
+  "cow",
+  "elephant",
+  "bear",
+  "zebra",
+  "giraffe",
+  "backpack",
+  "umbrella",
+  "handbag",
+  "tie",
+  "suitcase",
+  "frisbee",
+  "skis",
+  "snowboard",
+  "sports ball",
+  "kite",
+  "baseball bat",
+  "baseball glove",
+  "skateboard",
+  "surfboard",
+  "tennis racket",
+  "bottle",
+  "wine glass",
+  "cup",
+  "fork",
+  "knife",
+  "spoon",
+  "bowl",
+  "banana",
+  "apple",
+  "sandwich",
+  "orange",
+  "broccoli",
+  "carrot",
+  "hot dog",
+  "pizza",
+  "donut",
+  "cake",
+  "chair",
+  "couch",
+  "potted plant",
+  "bed",
+  "dining table",
+  "toilet",
+  "tv",
+  "laptop",
+  "mouse",
+  "remote",
+  "keyboard",
+  "cell phone",
+  "microwave",
+  "oven",
+  "toaster",
+  "sink",
+  "refrigerator",
+  "book",
+  "clock",
+  "vase",
+  "scissors",
+  "teddy bear",
+  "hair drier",
+  "toothbrush"
+];
+
+export const loadModel = async () => {
+  return await tf.loadGraphModel("file://yolov5s/model.json");
+};
+
+export const detect = async (model: tf.GraphModel, data: string) => {
+  const [modelWidth, modelHeight] = model.inputs[0].shape!.slice(1, 3);
+  const base64 = data.replace(/^data:image\/jpeg;base64,/, "");
+  const image = await sharp(Buffer.from(base64, "base64")).resize(modelWidth, modelHeight).toBuffer();
+
+  const input1 = tf.node.decodeImage(image, 3);
+  const input2 = input1.expandDims(0);
+  const input3 = input2.toFloat();
+  const input4 = input3.div(255);
+  const output = await model.executeAsync(input4) as tf.Tensor[];
+
+  const boxes = await output[0].array() as number[][][];
+  const scores = await output[1].array() as number[][];
+  const classes = await output[2].array() as number[][];
+  const numDetections = (await output[3].array() as number[])[0];
+
+  const result = [];
+  for (let i = 0; i < numDetections; ++i) {
+    const [y1, x1, y2, x2] = boxes[0][i];
+    const score = scores[0][i];
+    const label = cocoNames[classes[0][i]];
+
+    result.push({ y1, x1, y2, x2, score, label });
+  }
+
+  // TF sucks!
+  tf.dispose(output);
+  tf.dispose(input4);
+  tf.dispose(input3);
+  tf.dispose(input2);
+  tf.dispose(input1);
+
+  return result;
+};
