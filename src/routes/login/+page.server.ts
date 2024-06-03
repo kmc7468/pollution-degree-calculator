18cde7456226ed8fdf327bdb6b0c0c33fcdb1e19
static <kmc7468@naver.com>
2024-06-04 00:48:09 +0900

    로그인 페이지 구현

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,44 @@
+import { error, fail, redirect } from "@sveltejs/kit";
+
+import { phoneNumberRegex } from "$lib/patterns";
+import { tryUserLogin } from "$lib/server/database";
+import { generateUserToken, verifyUserToken } from "$lib/server/jwt";
+
+/** @type {import("./$types").PageServerLoad} */
+export const load = async ({ cookies }) => {
+  const jwt = cookies.get("auth");
+  if (jwt && verifyUserToken(jwt)) {
+    redirect(302, "/mypage");
+  }
+};
+
+/** @type {import("./$types").Actions} */
+export const actions = {
+  default: async ({ request, cookies }) => {
+    const form = await request.formData();
+    const phoneNumber = form.get("phoneNumber")?.toString();
+    const password = form.get("password")?.toString();
+    if (!phoneNumber) {
+      error(400, "Phone number is required");
+    } else if (!phoneNumberRegex.test(phoneNumber)) {
+      error(400, "Invalid phone number");
+    } else if (!password) {
+      error(400, "Password is required");
+    } else if (password.length < 4) {
+      error(400, "Password is too short");
+    }
+
+    if (tryUserLogin(phoneNumber, password)) {
+      cookies.set("auth", generateUserToken(phoneNumber), {
+        path: "/",
+      });
+      redirect(302, "/mypage");
+    } else {
+      return fail(401, {
+        phoneNumber,
+        password,
+        message: "휴대폰 번호 또는 비밀번호가 올바르지 않아요."
+      });
+    }
+  }
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -8,41 +8,63 @@
   <script src="/assets/js/baguetteBox.min.js" defer></script>
   <script src="/assets/js/vanilla-zoom.js" defer></script>
   <script src="/assets/js/theme.js" defer></script>
-  <script src="/assets/js/login.js" defer></script>
 </svelte:head>
 
+<script lang="ts">
+  import { phoneNumberRegex } from "$lib/patterns";
+
+  /** @type {import("./$types").ActionData} */
+  export let form;
+</script>
+
 <nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar">
-  <div class="container"><a class="navbar-brand logo" href="#">분리수거101</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-    <div class="collapse navbar-collapse" id="navcol-1">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-        <li class="nav-item"><a class="nav-link active" href="/login">Login</a></li>
-        <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
-      </ul>
-    </div>
+<div class="container">
+  <a class="navbar-brand logo" href="/">분리수거 101</a>
+  <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1">
+    <span class="visually-hidden">Toggle navigation</span>
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navcol-1">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+      <li class="nav-item"><a class="nav-link active" href="/login">Login</a></li>
+      <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+    </ul>
   </div>
+</div>
 </nav>
 
 <main class="page">
-  <section class="clean-block clean-form dark">
-    <div class="container">
-      <div class="block-heading">
-        <h2 class="text-info">Log In</h2>
-        <p>서비스 사용기록 확인을 위해 로그인합니다.</p>
-      </div>
-      <form>
-        <div class="mb-3"><label class="form-label" for="email">ID</label><input class="form-control item" type="email" id="idBox" data-bs-theme="light"></div>
-        <div class="mb-3"><label class="form-label" for="password">Password</label><input class="form-control" type="password" id="password" data-bs-theme="light"></div>
-        <div class="mb-3">
-            <div class="form-check"><input class="form-check-input" type="checkbox" id="checkbox" data-bs-theme="light"><label class="form-check-label" for="checkbox">Remember me</label></div>
-        </div><button class="btn btn-primary" id="loginBTN" type="submit">Log In</button>
-      </form>
+<section class="clean-block clean-form dark">
+<div class="container">
+  <div class="block-heading">
+    <h2 class="text-info">로그인</h2>
+    <p>포인트 적립 기록을 확인하려면 로그인이 필요해요.</p>
+  </div>
+  <form method="POST">
+    <div class="mb-3">
+      <label class="form-label" for="phoneNumber">휴대폰 번호</label>
+      <input class="form-control item" type="tel" data-bs-theme="light"
+             name="phoneNumber" value={form?.phoneNumber ?? ""}
+             required pattern={phoneNumberRegex.source}>
     </div>
-  </section>
+    <div class="mb-3">
+      <label class="form-label" for="password">비밀번호</label>
+      <input class="form-control" type="password" data-bs-theme="light"
+             name="password" value={form?.password ?? ""}
+             required minlength=4>
+    </div>
+    {#if form?.message}
+      <div class="alert alert-danger" role="alert">{form.message}</div>
+    {/if}
+    <button class="btn btn-primary" type="submit">로그인</button>
+  </form>
+</div>
+</section>
 </main>
 
 <footer class="page-footer dark">
-  <div class="footer-copyright">
-    <p>© 2024 Copyright StartUp101-B-01</p>
-  </div>
+<div class="footer-copyright">
+  <p>© 2024 Copyright StartUp101-B-01</p>
+</div>
 </footer>
